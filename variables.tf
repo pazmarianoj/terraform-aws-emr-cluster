9db17c921ab5d14c160d53aab9a0f2de620dd30f
Daniel Hill <danhill@digital.uc.dwp.gov.uk>
2019-01-07 15:36:35 +0000

    allow setting additional security groups for service and nodes

--- variables.tf
+++ variables.tf
@@ -25,6 +25,10 @@ variable "key_name" {}
 
 variable "subnet_id" {}
 
+variable "service_access_security_group_id" {
+  description = "Security group with access to AWS Services over internet gateway. Applied to cluster service, master and slaves"
+}
+
 variable "custum_policy_count" {
   default = 0
   description = "Number of custom policy arns in custom_policy_arns"
